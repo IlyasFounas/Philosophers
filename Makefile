52aabed756ef24893b43435a95d4aaee1d63fe12
ifounas <founas.ilyas@gmail.com>
2025-03-03 16:54:48 +0100

    🚀 Start : Makefile added

---
+++ Makefile
@@ -0,0 +1,43 @@
+NAME    = philosophers
+SRC     = srcs/philosophers.c 
+
+
+MAKEFLAGS += --no-print-directory
+
+OBJ_DIR = objs
+OBJ     = $(addprefix $(OBJ_DIR)/, $(notdir $(SRC:.c=.o)))
+CFLAGS  = -Wall -Wextra -Werror -g3 -Ilibft -Iincludes
+CC      = cc
+
+LIBFT_DIR = libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+$(NAME): $(LIBFT) $(OBJ) includes/philosophers.h
+	$(CC) $(OBJ) $(LIBFT) -o $(NAME)
+
+$(OBJ_DIR)/%.o: srcs/%.c includes/philosophers.h | $(OBJ_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFT): force
+	make -C $(LIBFT_DIR)
+
+$(OBJ_DIR):
+		mkdir -p $(OBJ_DIR)
+
+clean:
+	rm -rf $(OBJ_DIR)
+	make clean -C $(LIBFT_DIR)
+
+fclean:
+	rm -rf $(OBJ_DIR)
+	rm -f $(NAME)
+	make fclean -C $(LIBFT_DIR)
+
+re: fclean all
+
+all: $(NAME)
+
+force:
+
+.PHONY: all clean fclean re force
+
---
+++ includes/philosophers.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ifounas <ifounas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 16:45:33 by ifounas           #+#    #+#             */
+/*   Updated: 2025/03/03 16:46:22 by ifounas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <pthread.h>
+
+#endif
